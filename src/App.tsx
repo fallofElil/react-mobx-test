b9ab55a930133366b96df8dd8f64194493cca632
Ilya Kiyanov <ikiyanov@protonmail.com>
2020-10-12 15:00:54 +0300

    removes eslint and prettier, stable

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React, { FC } from 'react';
+import 'antd/dist/antd.css';
+import { Button } from 'antd';
+
 import './App.css';
 
 const App: FC = () => {
   return (
     <div className="App">
-      <p>APP</p>
+      <Button type="primary">
+        Sample Button
+      </Button>
     </div>
   );
 };
