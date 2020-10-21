17df7297aca1b061b96793bf847bb53b83803315
Ilya Kiyanov <ikiyanov@protonmail.com>
2020-10-21 14:55:16 +0300

    changes ListItemStore structure

--- src/store/list-item.ts
+++ src/store/list-item.ts
@@ -1,40 +1,85 @@
-import { observable, action, makeObservable } from 'mobx';
+import { observable, action, computed, makeObservable } from 'mobx';
 import shortid from 'shortid';
 
 export interface IListItemStore {
-  id?: string;
-  title: string;
-  description: string;
-  visible: boolean;
-  setTitle(title: string): void;
-  setDescription(description: string): void;
-  setVisible(visible: boolean): void;
+  _id: string;
+  _title: string;
+  _description: string;
+  _visible: boolean;
+  // title(): string;
+  // setTitle(title: string): void;
+  // setDescription(description: string): void;
+  // setVisible(visible: boolean): void;
 }
 
 class ListItemStore implements IListItemStore {
-  readonly id: string = shortid();
-  title = 'Нет заголовка';
-  description = '';
-  visible = true;
+  private _initialState: IListItemStore = {
+    _id: '',
+    _title: '',
+    _description: '',
+    _visible: true,
+  };
+
+  private _id: string = this._initialState._id;
+  private _title: string = this._initialState._title;
+  private _description: string = this._initialState._description;
+  private _visible: boolean = this._initialState._visible;
 
   constructor() {
+    this._id = shortid();
+
     makeObservable(this, {
-      title: observable,
-      description: observable,
-      visible: observable,
+      _id: observable,
+      id: computed,
+      _title: observable,
+      title: computed,
+      setTitle: action,
+      _description: observable,
+      description: computed,
+      setDescription: action,
+      _visible: observable,
+      visible: computed,
+      setVisible: action,
     });
   }
 
-  @action setDescription(description: string): void {
-    this.description = description;
+  public get id(): string {
+    return this._id;
   }
 
-  @action setTitle(title: string): void {
-    this.title = title;
+  // public setId(id: string) {
+  //   this._id = id;
+  // }
+
+  public get title(): string {
+    return this._title;
   }
 
-  @action setVisible(visible: boolean): void {
-    this.visible = visible;
+  public setTitle(title: string): void {
+    this._title = title;
+  }
+
+  public get description(): string {
+    return this._description;
+  }
+
+  public setDescription(description: string): void {
+    this._description = description;
+  }
+
+  public get visible(): boolean {
+    return this._visible;
+  }
+
+  public setVisible(visible: boolean): void {
+    this._visible = visible;
+  }
+
+  public setInitialState(): void {
+    this._id = this._initialState._id;
+    this._title = this._initialState._title;
+    this._description = this._initialState._description;
+    this._visible = this._initialState._visible;
   }
 }
 
